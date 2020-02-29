ae2e14d3b38e480b722589262d96e5d63ec6275c
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-02-29 13:47:24 -0600

    added bear cards

--- helpers/bearData.js
+++ helpers/bearData.js
@@ -1,9 +1,23 @@
-
-
-const dinos = [
+const bears = [
     {
-      id: '',
-      name: '',
-      imageUrl: '',
-    },
+        name: 'Stephen',
+        imageUrl: 'https://cottagelife.com/wp-content/uploads/2018/06/animal-bear-brown-bear-35435-1200x796.jpg',
+        id: 'bear3',
+    }
 ];
+
+let selectedBearId = '';
+  
+  const setSelectedBear = (bearId) => {
+    selectedBearId = bearId;
+    console.log('selectedBearId', selectedBearId);
+  };
+  
+  const getBear = () => {
+    return bears;
+  };
+  
+  export default { setSelectedBear, getBear };
+
+
+
--- main.js
+++ main.js
@@ -1,69 +1,33 @@
-import util from './helpers/util.js';
+import river from './components/river.js'
+import bearForm from './components/bearForm.js';
 
 
-const bears = [
-    {
-        name: 'Stephen',
-        imageUrl: 'https://cottagelife.com/wp-content/uploads/2018/06/animal-bear-brown-bear-35435-1200x796.jpg',
-        id: 'bear1',
-    }
-];
+const init = () => {
+
+  bearForm.bearFormBuilder();
+  river.bearBuilder();
+    //bears.newBear();
+   
+    
+    // add event listener
+  river.bearButtonEvent();
+    // user submits a bear
+
+    // rerun bear builder
+  
+  };
+  
+  init();
+  
 
     
-// const selectBear = (e) => {
-//     const bearId = e.target.id;
-//     console.log(bearId)
-
-//   }
 
 
-// const bearSubmitEvent = (e) => {
-//     const bearButtons = document.getElementsByClassName('new-Bear');
-//     for(let i=0; i<bearButtons.length; i++){
-//       bearButtons[i].addEventListener('click', selectBear);
-//     }
-//   }
+
+
    
     
     
-// const bearFormBuilder = () => {
-//     let domString = ``
-//     for(let i=0; i < bears.length; i++) {
-//        domString += '<form>'
-//        domString += '<div class="row">'
-//        domString += '<div class="col">'
-//        domString += `<input type="text" class="form-control" placeholder="${bears.name}"></div>`
-//        domString += '<div class="col">'
-//        domString += `<input type="text" class="form-control" placeholder="${bears.imageUrl}"></div>`
-//        domString += '</div>'
-//        domString += '</form>'
-//        domString += `<button type="button" id="${bears.id}" class="btn btn-primary new-Bear">New Bear</button>`
-    
-//        util.printToDom('bear-form', domString)
-//        document.getElementById(bears.id).addEventListener('click', bearFormBuilder);
-//        console.log()
 
-//     }
-// };
-//     bearFormBuilder();
-
-const bearBuilder = () => {
-    let domString = ''
-    for (let i =0; i < bears.length; i++) {
-      domString += '<div class="card" style="width: 18rem;">'
-      domString += `<img src="${bears.imageUrl}" class="card-img-top" alt="...">`
-      domString += '<div class="card-body">'
-      domString += `<h5 class="card-title">${bears.name}</h5>`
-      domString += '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>'
-      domString += '<a href="#" class="btn btn-primary">Go somewhere</a>'
-      domString += '</div>'
-      domString += '</div>'
-
-      util.printToDom('bear-card', domString)
-// document.getElementById('').addEventListener('click', bearBuilder);
- }
-};
-
-bearBuilder();
 
     
